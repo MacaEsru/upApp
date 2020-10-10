de02212221f063e1388e35c6778065d907ae89b1
MacaEsru <163170@ids.upchiapas.edu.mx>
2020-10-10 14:32:30 -0500

    add main and activity login

---
+++ app/src/main/java/com/example/upapp/ui/login/main.java
@@ -0,0 +1,14 @@
+package com.example.upapp.ui.login;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.upapp.R;
+
+public class main extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
